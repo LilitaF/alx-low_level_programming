24a484cfd91a509547550ba78d3ff553dd501d06
Lilita <lilitambete2911@gmail.com>
2023-07-17 16:28:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * return - always 0 (success)
  */
 int main(void)
-{	//reutrn value will be 0 (success)
+{	/*return value will be 0 (success)*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
