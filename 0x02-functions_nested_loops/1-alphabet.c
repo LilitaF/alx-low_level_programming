398fca94a4ab01678fd5beb6b452156c9f8415ba
Lilita <lilitambete2911@gmail.com>
2023-07-18 14:04:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * print_alphabet - prints alphabet in lowercase
+ * Return: 0 if successful otherwise 1 upon error
+ */
+int main(void)
+void print_alphabet(void);
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
