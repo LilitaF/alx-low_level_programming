dccf68a18da067fe60f1dbb387308433102eee2c
Lilita <lilitambete2911@gmail.com>
2023-07-19 16:37:57 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - functions that find and prints natural number
+ * @n: starting point of natural numbers.
+ * Return: noting to return.
+ */
+void print_to_98(int n)
+{
+	while (n >= 98)
+	{
+		_putchar("%d ,\n", n)
+			n++;
+	}
+}
