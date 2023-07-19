441ced7429a1b901d8d40572664ff989a00e1ef4
Lilita <lilitambete2911@gmail.com>
2023-07-19 14:20:33 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - function that created a times table.
+ * Return: has no return (void).
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			c = a * b;
+
+			if (c <= 9)
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
