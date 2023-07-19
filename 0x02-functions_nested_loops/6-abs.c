b43c16e91a99645f4dfda57ade2a074db9b15181
Lilita <lilitambete2911@gmail.com>
2023-07-18 20:30:20 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @int: interger peremeter
+ * Return: 0 if successful otherwise 1 upon error.
+ */
+int _abs(int)
+{
+	int a;
+
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+	return (0);
+}
