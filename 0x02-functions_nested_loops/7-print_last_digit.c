9e93effdd006ecced30decc247b85c3389123292
Lilita <lilitambete2911@gmail.com>
2023-07-19 08:44:51 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_last_digit - function that finds the last digit of a number
- * @a: variable to be compared
+ * @a: input number to be compared
  * Return: 0 if successful otherwise 1 upon error
  */
 int print_last_digit(int a)
@@ -9,7 +9,6 @@ int print_last_digit(int a)
 	int b;
 
 	b = a % 10;
-
 	if (b < 0)
 	{
 		_putchar(-b + 0);
