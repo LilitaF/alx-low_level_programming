9dd72d8b12dd1c52a911827d33c3fcd516467cac
Lilita <lilitambete2911@gmail.com>
2023-07-20 14:41:39 -0700

    3-priint_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ * Return: has no return.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c);
+	_putchar('\n');
+}
