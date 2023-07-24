e7ed0eb792dd6bc8ff407939a863a0eb6b807a2e
Lilita <lilitambete2911@gmail.com>
2023-07-24 10:19:41 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: variable of the string in question
- * Return: always 0
+ * Return: q
  */
 int _strlen(char *s)
 {
 	int q;
 
-	for (q = 0; s[q] != '0'; ++q);
+	for (q = 0; s[q] != '\0'; ++q);
 
 	return (q);
 }
