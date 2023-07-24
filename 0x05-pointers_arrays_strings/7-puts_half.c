814c71438b2bd4ab5dc5cf173adb86bf09502a81
Lilita <lilitambete2911@gmail.com>
2023-07-24 15:21:56 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,3 +6,16 @@
  */
 void puts_half(char *str)
 {
+	int n;
+	int a;
+	int length = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		length++;
+
+	n = (length - 1)/ 2;
+
+	for (a = n + 1; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
