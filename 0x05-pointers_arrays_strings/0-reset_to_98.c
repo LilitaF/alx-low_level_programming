dc66be86597c4a96857ed77cfe7d435e048d93f5
Lilita <lilitambete2911@gmail.com>
2023-07-24 08:39:16 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of a pointer to an int to 98
+ * @n: pointer to an int as a parameter
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains files that are directly used for learning anout pointers, arrays and strings. As well as how to use and manipulate them.
