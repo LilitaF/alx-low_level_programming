67b7b2f7c61a30de745463385d6306c5c7720c6d
Lilita <lilitambete2911@gmail.com>
2023-08-02 09:38:56 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,20 +9,18 @@ void _print_rev_recursion(char *s)
 	int a = 0; /*created and given a value cause s is a pointer*/
 
 	/**
-	 * NULL is at the beginning this time thus if printing a new
-	 * line when NULL is seen, the new line will be printed first
-	 * then the string.
-	 * to prevent this s[a] should not be equal to NULL
-	 * to print a new line after the reversed string
+	 * NULL is at the beginning this time so as long as s[a]
+	 * has not seen NULL yet print the reverse of the string
+	 * otherwise return nothing
 	 */
 
-	if (s[a] == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+	if (s[a] != '\0')
 	{
 		_print_rev_recursion(s + (a + 1));
 		_putchar(s[a]);
+	}
+	else
+	{
+		return ;
 	}
 }
