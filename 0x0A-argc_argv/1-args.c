9a340b5a29ef9055ba65d8ff5e3d4717dbbafe45
Lilita <lilitambete2911@gmail.com>
2023-08-04 14:03:45 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argv: argument vector, prints command line arguments
+ * @argc: argument count, counts command line arguments
+ * Return: 0 if successful. 1 upon error
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
