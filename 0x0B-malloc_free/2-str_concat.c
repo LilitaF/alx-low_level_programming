590f09ce03790d0703ac390fc76322026d9fa83a
Lilita <lilitambete2911@gmail.com>
2023-08-08 15:21:51 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,18 @@ char *str_concat(char *s1, char *s2)
 	int a;
 	int len1 = strlen(s1);
 	int len2 = strlen(s2);
-	char *s3 = malloc(((len1 + len2) + 1) * sizeof(char));
+	char *s3;
 
-	if (s1 == NULL || s2 == NULL)
-		return ("");
+	s3 = (char *) malloc(((len1 + len2) + 1) * sizeof(char));
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
 	for (a = 0; a < len1; a++)
 	{
@@ -27,6 +35,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		s3[len1 + a] = s2[a];
 	}
-	s3[(len1 + len2 + 1) - 1] = '\0';
+	s3[(len1 + len2 + 1)] = '\0';
 	return (s3);
 }
