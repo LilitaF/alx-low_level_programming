578bb231a79fc2a3f288deacc41550dbc3db3487
Lilita <lilitambete2911@gmail.com>
2023-08-09 16:17:44 -0700

    4-add.c fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,15 +13,29 @@ int main(int argc, char *argv[])
 {
 	int a = 1;
 	int num = 0;
+	/*used to access the argv array which has stored pointrs*/
+	char *p;
+	int b;
 
 	if (argc < 1)
 	{
 		printf("0\n");
 	}
-	if (*argv[a] < '0' || *argv[a] > '9')
+	else
 	{
-		printf("Error\n");
-		return (1);
+		for (a = 1; a < argc; a++)
+		{
+			p = argv[a];
+
+			for (b = 0; p[b] != '\0'; b++)
+			{
+				if (*argv[a] < '0' || *argv[a] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
 	}
 	for (a = 1; a < argc; a++)
 	{
