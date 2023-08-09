6f3815d440ee6860721051252d01ce3c75ffaa2e
Lilita <lilitambete2911@gmail.com>
2023-08-09 09:17:21 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argv: argument vector, prints string command line argument
+ * @argc: argument count, counts command line agrument
+ * Return: 0 if successful 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int a;
+	int num3;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (a = 0; a < argc; a++)
+	{
+		num3 = num1 * num2;
+	}
+	printf("%d\n", num3);
+	return (0);
+}
