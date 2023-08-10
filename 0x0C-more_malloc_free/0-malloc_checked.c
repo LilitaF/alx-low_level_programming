17f2eec3e62290dbe38ca3e9d06fab6239e7b9ec
Lilita <lilitambete2911@gmail.com>
2023-08-10 06:49:04 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: int that needs memory allocation
+ * Return: pointer if successful and 98 if it fails.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(unsigned int) * b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr)
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+This project is similar to the previous one. It is meant to allow us to further understand the concept of malloc and how it works.
+
+This project contains 4 mandatory tasks
