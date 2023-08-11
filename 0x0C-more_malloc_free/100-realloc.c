aa1471672811be4b6a0c7428051214f84edf394a
Lilita <lilitambete2911@gmail.com>
2023-08-11 11:59:47 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	/*changed typecast from void to char for access*/
 	char *ptrsub = ptr;
 
-	if (new_size == 0)
+	if (new_size == old_size)
 	{
 		return (ptr);
 	}
