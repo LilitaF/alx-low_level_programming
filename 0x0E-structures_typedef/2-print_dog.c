f2e417be662b709ae770febcc5087de01717a00c
Lilita <lilitambete2911@gmail.com>
2023-08-15 05:17:18 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,13 @@ void print_dog(struct dog *d)
 		return;
 	}
 	if ((*d).name == NULL)
+	{
 		printf("Name: (nil)\n");
+	}
 	if ((*d).owner == NULL)
+	{
 		printf("Owner: (nil)\n");
+	}
 
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
