08bafeacc30255558fee4ed04349558a2c16446f
Lilita <lilitambete2911@gmail.com>
2023-08-16 13:54:30 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to a function
+ * Return: has no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+This project is aimed at learning and understanding function pointers.
+There are 4 tasks aimed at doing this.
