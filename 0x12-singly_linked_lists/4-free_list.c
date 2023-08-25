78f8f0a983c4e163c3f8de1d3a4988d4bcfe1be7
Lilita <lilitambete2911@gmail.com>
2023-08-24 17:27:30 -0700

    4-free_lists.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,24 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * free_list - frees a list_t list.
  * @head: pointer to the first element thus list_t
  * Return: has no return
  */
 void free_list(list_t *head)
+{
+	list_t *curr = head;/*initialised to be used to loop*/
+	list_t *next_node; /*used so that we dont lose the next pointer/node*/
+
+	while (curr != NULL)
+	{
+		next_node = curr->next;/*storing the next pointer*/
+		free(curr->str);
+		free(curr);
+		curr = next_node; /*moving to the next node*/
+	}
+	/*we need to handle the head pointer to show the list is empty*/
+	head = NULL;
+}
