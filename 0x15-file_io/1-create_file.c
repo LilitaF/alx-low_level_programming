9eb0c42643481c0c09b2a07d2dd105530476720c
Lilita <lilitambete2911@gmail.com>
2023-09-04 18:01:54 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,11 +19,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1); /*creation failed*/
-	while (text_content[len])
-		len++;
 
 	if (text_content != NULL)
 	{
+		while (text_content[len])
+			len++;
+
 		write_return = write(fd, text_content, len);
 		if (write_return == -1)
 		{
