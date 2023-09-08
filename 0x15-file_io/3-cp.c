981a5c6af9993dd029704647900a097011ef0bcd
Lilita <lilitambete2911@gmail.com>
2023-09-08 09:35:40 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,18 @@
 #include "main.h"
 #define BUFFSIZE 1024
 /**
+ * exit_error - prints error message for main function
+ * @code: code to be displayed uppon error
+ * @message: message to be printed when exit code appears
+ * @arg: argument for which the exit code and message is for
+ * Return: has no return
+ */
+void exit_error(int code, const char *message, const char *arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
+}
+/**
  * main - entry point, function to copy from argv[1] to [2]
  * @argc: argument count
  * @argv: argument vector
@@ -13,37 +25,33 @@ int main(int argc, char *argv[])
 	char buffer[BUFFSIZE];
 	ssize_t read_bytes, written_bytes;
 
-	if (argc > 3)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		exit_error(97, "Usage: cp file_from file_to\n", "");
 	}
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
+		exit_error(98, "Error: Can't read from file %s\n", file_from);
 	}
 	fd2 = open(argv[2], O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP
 			| S_IWGRP | S_IROTH);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
+		exit_error(99, "Error: Can't write to %s\n", file_to);
 	}
 	while ((read_bytes = read(fd1, buffer, BUFFSIZE)) > 0)
 	{
 		written_bytes = write(fd2, buffer, read_bytes);
 		if (written_bytes != read_bytes)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_to);
-			exit(99);
+			exit_error(99, "Error: Can't write from file %s\n", file_to);
 		}
 	}
 	if (read_bytes == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file '%s'\n", file_from);
-		exit(98);
+		exit(99);
 	}
 	if (close(fd1) == -1)
 	{
